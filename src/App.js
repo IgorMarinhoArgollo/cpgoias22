f108e84b173cb4155f4d0f6f72f9318767c87436
IgorMarinhoArgollo <igormarinhoargollo@gmail.com>
2022-06-16 19:40:12 -0300

    criando as rotas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Video from './Pages/Video';
+import NotFound from './Pages/NotFound';
+
 
 function App() {
   return (
-    <div className="Home">
-      
-    </div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path='/video' component={Video} />
+      <Route path="*" component={NotFound} />
+    </Switch>
   );
 }
 
